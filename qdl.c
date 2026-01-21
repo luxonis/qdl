435e23094d3a39fcc55531d77cfdc5ca9b8b4349
Danilo <danilo.pejovic@luxonis.com>
2026-01-21 13:44:18 +0100

    checkpatch

--- qdl.c
+++ qdl.c
@@ -421,66 +421,66 @@ static int qdl_reset(struct qdl_device *qdl,
 			     int argc, char **argv,
 			     int optind)
 {
-    struct sahara_image prog = {0};
-    int ret;
+	struct sahara_image prog = {0};
+	int ret;
 
-    ret = qdl_open(qdl, serial);
-    if (ret)
-	return ret;
+	ret = qdl_open(qdl, serial);
+	if (ret)
+		return ret;
 
-    qdl->storage_type = storage_type;
+	qdl->storage_type = storage_type;
 
-    fprintf(stderr, "[reset] entering reset path\n");
+	fprintf(stderr, "[reset] entering reset path\n");
 
-    if (optind >= argc) {
-	fprintf(stderr, "[reset] ERROR: no programmer positional arg.\n");
-	fprintf(stderr, "[reset] Example:\n");
-	fprintf(stderr, " qdl --reset --storage ufs xbl_s_devprg_ns.melf\n");
-	return -1;
-    }
+	if (optind >= argc) {
+		fprintf(stderr, "[reset] ERROR: no programmer positional arg.\n");
+		fprintf(stderr, "[reset] Example:\n");
+		fprintf(stderr, " qdl --reset --storage ufs xbl_s_devprg_ns.melf\n");
+		return -1;
+	}
 
-    const char *prog_path = argv[optind];
+	const char *prog_path = argv[optind];
 
-    fprintf(stderr, "[reset] loading programmer: %s\n", prog_path);
+	fprintf(stderr, "[reset] loading programmer: %s\n", prog_path);
 
-    ret = load_sahara_image(prog_path, &prog);
-    if (ret < 0) {
-	fprintf(stderr, "[reset] ERROR: load_sahara_image failed\n");
-	return ret;
-    }
-    prog.name = prog_path;
+	ret = load_sahara_image(prog_path, &prog);
+	if (ret < 0) {
+		fprintf(stderr, "[reset] ERROR: load_sahara_image failed\n");
+		return ret;
+	}
+	prog.name = prog_path;
 
-    fprintf(stderr,
-	    "[reset] programmer loaded: name=%s len=%zu ptr=%p\n",
-	    prog.name, prog.len, prog.ptr);
-
-    if (!prog.ptr || prog.len == 0) {
-	fprintf(stderr, "[reset] ERROR: programmer buffer invalid\n");
-	return -1;
-    }
-
-    if (!qdl || !qdl->read || !qdl->write) {
 	fprintf(stderr,
-		"[reset] ERROR: qdl device not initialized/opened "
-		"(qdl=%p read=%p write=%p)\n",
-		(void *)qdl,
-		qdl ? (void *)qdl->read : NULL,
-		qdl ? (void *)qdl->write : NULL);
-	return -1;
-    }
+		"[reset] programmer loaded: name=%s len=%zu ptr=%p\n",
+		prog.name, prog.len, prog.ptr);
 
-    fprintf(stderr, "[reset] running Sahara (upload programmer)\n");
-    ret = sahara_run(qdl, &prog, true, NULL, NULL);
-    if (ret < 0) {
-	fprintf(stderr, "[reset] ERROR: sahara_run failed\n");
+	if (!prog.ptr || prog.len == 0) {
+		fprintf(stderr, "[reset] ERROR: programmer buffer invalid\n");
+		return -1;
+	}
+
+	if (!qdl || !qdl->read || !qdl->write) {
+		fprintf(stderr,
+			"[reset] ERROR: qdl device not initialized/opened "
+			"(qdl=%p read=%p write=%p)\n",
+			(void *)qdl,
+			qdl ? (void *)qdl->read : NULL,
+			qdl ? (void *)qdl->write : NULL);
+		return -1;
+	}
+
+	fprintf(stderr, "[reset] running Sahara (upload programmer)\n");
+	ret = sahara_run(qdl, &prog, true, NULL, NULL);
+	if (ret < 0) {
+		fprintf(stderr, "[reset] ERROR: sahara_run failed\n");
+		return ret;
+	}
+
+	fprintf(stderr, "[reset] Sahara done; requesting Firehose reset\n");
+	ret = firehose_request_reset(qdl);
+	fprintf(stderr, "[reset] done; ret=%d\n", ret);
+
 	return ret;
-    }
-
-    fprintf(stderr, "[reset] Sahara done; requesting Firehose reset\n");
-    ret = firehose_request_reset(qdl);
-    fprintf(stderr, "[reset] done; ret=%d\n", ret);
-
-    return ret;
 }
 
 static void print_usage(FILE *out)
